2e1cbeeb09417c4ede0176606105c16dc7315ca5
DeveloperOfTheConch <kcalvertmail@gmail.com>
2024-08-26 17:29:41 -0400

    Initial Commit

---
+++ Initiative Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Initiative Tracker
+//
+//  Created by Dev Conch on 2024-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Initiative Tracker/Initiative_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Initiative_TrackerApp.swift
+//  Initiative Tracker
+//
+//  Created by Dev Conch on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct Initiative_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
